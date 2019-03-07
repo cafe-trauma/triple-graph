127d8285b50466a80db91b41be7d14b784ecfe44
Joseph Utecht <joseph.utecht@gmail.com>
2019-03-07 10:02:38 -0600

    fixed missing links added more info

--- draw.rb
+++ draw.rb
@@ -20,9 +20,10 @@ def find_label(uri, graph)
 end
 
 class Representation
-  attr_reader :question, :option
-  def initialize(question_id)
+  attr_reader :question, :option, :question_text
+  def initialize(question_id, question_text)
     @question = question_id
+    @question_text = question_text.scan(/([^{]+){([^|]+)|[^}]+}(.+)/).join()
   end
 end
 
@@ -57,13 +58,14 @@ class Anon
 end
 
 class Statement
-  attr_reader :representation, :subject, :predicate, :object
-  def initialize(rep, s, p, o, graph)
+  attr_reader :representation, :subject, :predicate, :object, :choice
+  def initialize(rep, s, p, o, choice, graph)
     @representation = rep
     @subject = s
     @predicate = p
     @p_label = find_label(p, graph)
     @object = o
+    @choice = !choice.nil?
   end
 
   def print
@@ -85,7 +87,7 @@ representations = []
 @statements = []
 
 query = <<SQL
-select subject, predicate, obj, question_id from questionnaire_statement
+select subject, predicate, obj, question_id, text, choice_id from questionnaire_statement
 join questionnaire_question on question_id = questionnaire_question.id
 join questionnaire_category on category_id = questionnaire_category.id
 where questionnaire = '#{questionnaire}'
@@ -94,7 +96,7 @@ SQL
 db_statements = DB[query].to_hash_groups(:question_id)
 db_statements.each do |question, statements|
   #next unless ARGV.include?(question.to_s)
-  rep = Representation.new(question)
+  rep = Representation.new(question, statements.first[:text])
   statements.select {|s| s[:predicate] == "rdf:type" }.each do |instance|
     uri = get_uri(instance[:subject], question)
     rdfclass = get_uri(instance[:obj], question)
@@ -108,7 +110,7 @@ db_statements.each do |question, statements|
     si ||= Anon.new(s)
     oi = @instances[o]
     oi ||= Anon.new(o)
-    @statements << Statement.new(rep, si, p, oi, @graph)
+    @statements << Statement.new(rep, si, p, oi, statement[:choice_id], @graph)
   end
 end
 
@@ -123,15 +125,27 @@ EOS
 
 File.write("#{questionnaire}.dot", dot)
 
+def find_or_create_node(i, nodes, rep)
+  unless nodes.index {|n| n[:uri] == i.uri} then
+    nodes << {:uri => i.uri,
+              :type => "ANON",
+              :question => rep.question,
+              :q_text => rep.question_text}
+  end
+  return i.uri
+end
+
 json = {}
 json["nodes"] = []
 json["links"] = []
 @instances.each do |k, v|
-  json["nodes"] << {:uri => k, :type => "INSTANCE", :label => v.label}
+  json["nodes"] << {:uri => k,
+                    :type => "INSTANCE",
+                    :label => v.label}
 end
 @statements.each do |s|
-  json["links"] << {:source => s.subject.print, :target => s.object.print, :type => "A"}
-  json["nodes"] << {:uri => s.subject.print, :type => "ANON"} if s.subject.is_a?(Anon)
-  json["nodes"] << {:uri => s.object.print, :type => "ANON"} if s.object.is_a?(Anon)
+  json["links"] << {:source => find_or_create_node(s.subject, json["nodes"], s.representation),
+                    :target => find_or_create_node(s.object, json["nodes"], s.representation),
+                    :type => s.choice ? "B" : "A"}
 end
 File.write("#{questionnaire}.json", JSON.generate(json))
